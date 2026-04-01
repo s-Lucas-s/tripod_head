4bb5e83d808b10561556ea559d64eff00b236124
Veki_Wang <1526302178@qq.com>
2026-04-01 21:52:31 +0800

    feat(serial): 添加串口1初始化及数据vofa发送功能

--- System/sys.c
+++ System/sys.c
@@ -10,6 +10,13 @@ void nvic_init(void)
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     NVIC_InitTypeDef NVIC_InitStructure;
 
+    // USART1 (from Serial1.c)
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
     // USART2 (from board.c)
     NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
--- System/sys.h
+++ System/sys.h
@@ -11,6 +11,7 @@
 #include "board.h"
 #include "usart.h"
 #include "Serial.h"
+#include "Serial1.h"
 #include "Key.h"
 
 /* ————————————变量———————————— */
